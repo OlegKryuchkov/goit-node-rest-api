635feca52bd164e2252210ba3b57c7cda30b42be
OlegKryuchkov <kryuchkov.oleg@gmail.com>
2024-03-20 22:59:12 +0200

    hw-04 fixed

--- models/contactsModel.js
+++ models/contactsModel.js
@@ -35,16 +35,16 @@ export const createContactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
-  favorite: Joi.boolean().required(),
+  // favorite: Joi.boolean(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
   phone: Joi.string(),
-  favorite: Joi.boolean(),
+  // favorite: Joi.boolean(),
 });
 
 export const updateStatusContactSchema = Joi.object({
-  favorite: Joi.boolean().required(),
+  favorite: Joi.boolean(),
 });
